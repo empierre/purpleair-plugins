1c16a4493a9c079f1fb266626c96657920e5a84c
epierre <epierre@e-nef.com>
2017-09-27 22:30:24 +0200

    added P+H

--- get_purpleair.js
+++ get_purpleair.js
@@ -26,14 +26,22 @@ var parseResponse = function(response) {
 	var pm10=text.indexOf('PM 1.0');
 	var pm100=text.indexOf('PM 10');
 	var pc=text.indexOf('Particle counters');
+	var hum=text.indexOf('Humidity');
+	var pressure=text.indexOf('Pressure');
 	if(pm25 > -1) {
-		console.log(text.substr(pm25+7,pm10-pm25-6-8));
+		console.log("PM 2.5 "+text.substr(pm25+7,pm10-pm25-6-8));
 	}
 	if(pm10 > -1) {
-		console.log(text.substr(pm10+13,pm100-pm10-2));
+		console.log("PM 1.0 "+text.substr(pm10+13,pm100-pm10-2));
 	}
 	if(pm100 > -1) {
-		console.log(text.substr(pm100+18,pc-pm100-24));
+		console.log("PM 10  "+text.substr(pm100+18,pc-pm100-24));
+	}
+	if(hum > -1) {
+		console.log("Hum    "+text.substr(hum+9,2));
+	}
+	if(pressure > -1) {
+		console.log("Pres   "+text.substr(pressure+9,pm25-pressure-14));
 	}
   });
 
